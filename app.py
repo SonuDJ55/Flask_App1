b51c9f711a7a383e1c06573b4d8b356fd1498dec
Dhiraj <dhiraj.chavare123@gmail.com>
2025-02-12 17:04:32 +0530

    render_templet is added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #Create simple application with Flask framework"""
-from flask import Flask, redirect,url_for
+from flask import Flask, redirect,url_for, render_template
 
 #create Flask app
 app = Flask(__name__)
@@ -14,6 +14,13 @@ def Home_Page():
 @app.route('/redirect_to_homepage')
 def redirect_to_homepage():
     return redirect(url_for('Home_Page'))
+@app.route('/CarrerPage')
+def Carrer_Page():
+    return "Welcome to my career page"
+
+@app.route()
+def index():
+    return render_template('index.html')
    
    
 
